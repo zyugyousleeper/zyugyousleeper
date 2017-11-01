7cdf6fd1b7ae7ea717487ec870dfd0bee813eaf3
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-11-01 18:35:29 +0900

    [rm]普通にmain関数使ったほうが良さげ

--- src/test/ModeSelectorTest.java
+++ src/test/ModeSelectorTest.java
@@ -1,42 +1,14 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import ui.ModeSelector;
 
 class ModeSelectorTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-//		fail("Not yet implemented");
-		System.out.println("hello to test");
+	public static void main(String[] args) {
 		JFrame frame = new ModeSelector();
+		frame.add(new JButton());
 		frame.setVisible(true);
-		
 	}
-
 }

6d550e11ad7141d399e444455449a21b563daabf
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-11-10 14:45:13 +0900

    [fix]ファイル名を変更

--- src/test/ReadNFC.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class ReadNFC {
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/test/ReadNFCTest.java
@@ -0,0 +1,7 @@
+package test;
+
+public class ReadNFCTest {
+	public static void main(String[] args) {
+		System.out.println("hello readnfctest");
+	}
+}

7cc2ebfb185e23ae8e24e67585cce70fe9c364e0
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-11-15 16:13:34 +0900

    [add]各種関数追加

--- src/model/Money.java
+++ src/model/Money.java
@@ -10,4 +10,18 @@ public class Money {
 	public static final int THOUSAND_YEN = 1000;
 	public static final int TWOTHOUSAND_YEN = 2000;
 	public static final int FIVETHOUSAND_YEN = 5000;
+	
+	private int money = 0;
+	
+	public Money() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Money(int money) {
+		this.money = money;
+	}
+	
+	public int getMoney() {
+		return this.money;
+	}
 }

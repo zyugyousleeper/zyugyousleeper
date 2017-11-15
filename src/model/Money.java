3f612b09b6b900517f24ad38e5c0a897dbb2bb20
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-11-15 16:11:39 +0900

    [add]定数追加

---
+++ src/model/Money.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Money {
+	public static final int ONE_YEN = 1;
+	public static final int FIVE_YEN = 5;
+	public static final int TEN_YEN = 10;
+	public static final int FIFTY_YEN = 50;
+	public static final int HUNDRED_YEN = 100;
+	public static final int FIVEHUNDRED_YEN = 500;
+	public static final int THOUSAND_YEN = 1000;
+	public static final int TWOTHOUSAND_YEN = 2000;
+	public static final int FIVETHOUSAND_YEN = 5000;
+}

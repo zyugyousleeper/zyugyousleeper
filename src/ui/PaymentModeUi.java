075825c7d9d927179744c7399d3394e9c6781a94
sanoto <kmn2wei@yahoo.co.jp>
2017-11-16 13:28:22 +0900

    [add] 入金モードのUIを作成

---
+++ src/ui/PaymentModeUi.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class PaymentModeUi extends JFrame{
+	public PaymentModeUi() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+	}
+}

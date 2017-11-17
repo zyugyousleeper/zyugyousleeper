3818b1ae5124dceb1d05192dd8ab42aa36d4f6e1
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-11-17 18:47:35 +0900

    [fix]ContainTestに変更

--- src/test/ContainerTest.java
+++ src/test/ContainerTest.java
@@ -3,11 +3,12 @@ package test;
 import model.Item;
 import ui.Container;
 
-public class ModeSelectorTest {
+public class ContainerTest {
 	public static void main(String[] args) {
 		Container<Item> frame = new Container<>();
 		
 		Item mode = new Item();
+		mode.setText("hello");
 
 		frame.addMode(mode);
 		frame.addMode(mode);

095ecf5005e53d459b6b17b174deb679854a902f
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-12-12 15:24:09 +0900

    [add]layoutをちょっと追加

--- src/ui/ModeUi.java
+++ src/ui/ModeUi.java
@@ -1,7 +1,32 @@
 package ui;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JToolBar;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class ModeUi extends JFrame {
+	public ModeUi() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.YELLOW);
+		getContentPane().add(panel, BorderLayout.CENTER);
+		
+		JLabel fractionLabel = new JLabel("New label");
+		getContentPane().add(fractionLabel, BorderLayout.SOUTH);
+		fractionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel buttonPanel = new JPanel();
+		getContentPane().add(buttonPanel, BorderLayout.NORTH);
+		buttonPanel.setLayout(new BorderLayout(0, 0));
+		
+		JButton cancelButton = new JButton("New button");
+		buttonPanel.add(cancelButton, BorderLayout.EAST);
+	}
 
 }

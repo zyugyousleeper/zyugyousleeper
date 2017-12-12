bfadd004d65212b0eab8af8c36b80f67e3da2227
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-12-12 15:27:44 +0900

    [add]testを追加

---
+++ src/test/ModeUiTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.awt.Panel;
+
+import javax.swing.JFrame;
+
+import ui.ModeUi;
+
+public class ModeUiTest {
+	public static void main(String[] args) {
+		JFrame frame = new ModeUi();
+		frame.setVisible(true);
+	}
+}

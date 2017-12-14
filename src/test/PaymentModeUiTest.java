66ee70add564341506a2a89b4794d84d08f6595b
sanoto <kmn2wei@yahoo.co.jp>
2017-12-15 08:36:19 +0900

    [fix] テスト用ファイルの中身を変更

--- src/test/PaymentModeUiTest.java
+++ src/test/PaymentModeUiTest.java
@@ -13,6 +13,7 @@ public class PaymentModeUiTest {
 		necessaryMoney.addMoney(Money.TEN_YEN, 50);
 		PaymentModeUi paymentModeUi = new PaymentModeUi();
 		paymentModeUi.setNecessaryMoney(necessaryMoney);
+		paymentModeUi.setMoneyKinds(Money.HUNDRED_YEN, Money.THOUSAND_YEN);
 		frame.add(paymentModeUi);
 		frame.setVisible(true);
 		remainingMoney.addMoney(Money.TEN_YEN, 50);

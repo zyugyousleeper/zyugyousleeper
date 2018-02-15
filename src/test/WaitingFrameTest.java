3e82deb2cdc09ddb9f19089d1a072fe84ac1077f
ynakano1127 <ynakano1127@gmail.com>
2018-02-15 16:33:36 +0900

    [add]ファイルを追加

---
+++ src/test/WaitingFrameTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import javax.swing.JFrame;
+
+import ui.WaitingFrame;
+
+public class WaitingFrameTest {
+	public static void main(String[] args) {
+		JFrame frame = new WaitingFrame();
+		frame.setVisible(true);
+		
+	}
+}
---
+++ src/ui/WaitingFrame.java
@@ -0,0 +1,7 @@
+package ui;
+
+import wrapper.FrameWrapper;
+
+public class WaitingFrame extends FrameWrapper {
+
+}

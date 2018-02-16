1dfabdd9a2b138c95629d9e8e6036959c5753262
ynakano1127 <ynakano1127@gmail.com>
2018-02-16 14:59:19 +0900

    [add]ラベルを追加

--- src/ui/WaitingFrame.java
+++ src/ui/WaitingFrame.java
@@ -1,7 +1,19 @@
 package ui;
 
 import wrapper.FrameWrapper;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
 
 public class WaitingFrame extends FrameWrapper {
-
+	public WaitingFrame() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("カードをおいてください");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 40));
+		getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+	}
 }

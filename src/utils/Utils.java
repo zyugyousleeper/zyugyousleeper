464b95095feb0c6ff9794dd861d32c8f885e04b5
ynakano1127 <ynakano1127@gmail.com>
2018-02-20 15:50:35 +0900

    [add]utilのgetを実装

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,10 +1,44 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
+
 import model.User;
 
 public class Utils {
-	static User getUser(int userID) {
-		return new User();
+	static User getUser(int userID) throws Exception {
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("http://www.ekidata.jp/api/l/25001.json");
+		StringBuilder builder = new StringBuilder();
+		HttpResponse response = client.execute(httpGet);
+		
+		int statusCode = response.getStatusLine().getStatusCode();
+		if (statusCode != 200) throw new Exception();
+		
+		HttpEntity entity = response.getEntity();
+		InputStream content = entity.getContent();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+		}
+		
+		JSONObject jsonObject = new JSONObject(builder.toString());
+		
+		User user = new User();
+		user.setStudentNum(jsonObject.getInt("userID"));
+		user.setName(jsonObject.getString("name"));
+		user.setFelicaID(jsonObject.getString("felicaID"));
+			
+		return user;
 	}
 	
 	static void deleteUser(int userID) {

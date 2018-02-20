6cf3c352735a4c9db17387dc28313b4fe6e52fbd
ynakano1127 <ynakano1127@gmail.com>
2018-02-20 15:02:12 +0900

    [add]Humanクラスを作成

---
+++ src/model/Human.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Human {
+	private String name;
+	private int studentNum;
+	private String felicaID;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStudentNum() {
+		return studentNum;
+	}
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+	public String getFelicaID() {
+		return felicaID;
+	}
+	public void setFelicaID(String felicaID) {
+		this.felicaID = felicaID;
+	}
+	
+}

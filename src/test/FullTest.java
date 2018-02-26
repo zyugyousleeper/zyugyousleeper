9500a527b036db38ad9cccdd529ecbecc37154cc
ynakano1127 <ynakano1127@gmail.com>
2018-02-26 14:18:41 +0900

    [add]purchasemodeを追加

--- src/test/FullTest.java
+++ src/test/FullTest.java
@@ -2,13 +2,14 @@ package test;
 
 import model.items.modes.ChargeMode;
 import model.items.modes.Mode;
+import model.items.modes.PurchaseMode;
 import ui.Container;
 
 public class FullTest {
 	public static void main(String[] args) {
 		Container<Mode> container = new Container<>(); 
 		container.addItem(new ChargeMode());
-		
+		container.addItem(new PurchaseMode());
 		container.setVisible(true);
 	}
 }

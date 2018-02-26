959e1598bcbd42a709308670b100dbc20bb727bb
ynakano1127 <ynakano1127@gmail.com>
2018-02-26 11:00:33 +0900

    [fix]testを修正

--- src/test/ReadNFCTest.java
+++ src/test/ReadNFCTest.java
@@ -4,7 +4,16 @@ import model.NFCThread;
 
 public class ReadNFCTest {
 	public static void main(String[] args) {
-		NFCThread nfcThread = new NFCThread();
-		nfcThread.run();
+		try {
+			NFCThread nfcThread = new NFCThread();
+			nfcThread.start();
+			System.out.println("hello");
+			Thread.sleep(5000);
+			nfcThread.close();
+			System.out.println("hello");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }

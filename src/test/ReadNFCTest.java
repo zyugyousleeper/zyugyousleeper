dffdb3a0850e24fa47a0bda600c1438fbfb1fef3
ynakano1127 <ynakano1127@gmail.com>
2018-02-26 10:27:59 +0900

    [add]nfcのtestを追加

--- src/test/ReadNFCTest.java
+++ src/test/ReadNFCTest.java
@@ -5,24 +5,37 @@ import java.io.InputStreamReader;
 
 public class ReadNFCTest {
 	public static void main(String[] args) {
-		try {
-			String[] command = {"python", "felica2.py"};
-	        Runtime r = Runtime.getRuntime();
-			Process p = r.exec(command);
-			
-			System.out.println("hello");
-
-			BufferedReader r_std = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line_std = null;
-			StringBuilder stdOutput = new StringBuilder();
-			while ((line_std = r_std.readLine()) != null) {
-				stdOutput.append(line_std + "\n");
-			}
-			System.out.println(stdOutput.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-		}
+		MyThread myThread = new MyThread();
+		myThread.run();
 	}
 }
 
+class MyThread extends Thread{
+	@Override
+	public void run() {
+		super.run();
+		try {
+			while (true) {
+				String[] command = {"python","felica2-2.py"};
+		        Runtime r = Runtime.getRuntime();
+				Process p = r.exec(command);
+
+				int result = p.waitFor();
+				System.out.println(result);
+				
+				BufferedReader r_std = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				
+				String line_std = null;
+				StringBuilder stdOutput = new StringBuilder();
+				while ((line_std = r_std.readLine()) != null) {
+					stdOutput.append(line_std + "\n");
+				}
+				System.out.println(stdOutput.toString());
+				
+				Thread.sleep(1000);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}

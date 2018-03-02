602b7e11eed6fd177a376e779703c4167a9fe507
ynakano1127 <ynakano1127@gmail.com>
2018-03-02 09:45:47 +0900

    [fix]utilを拡張 とりあえずgetだけ

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
@@ -14,13 +15,12 @@ import org.json.JSONObject;
 import model.User;
 
 public class Utils {
-	public static User getUser(int userID) throws Exception {
-		String url = "http://192.168.1.171:8080/api/users/" + String.valueOf(userID) + "/";
+	
+	private static User HTTPExcute(HttpRequestBase reqest) throws Exception{
 		
 		CloseableHttpClient client = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(url);
 		StringBuilder builder = new StringBuilder();
-		HttpResponse response = client.execute(httpGet);
+		HttpResponse response = client.execute(reqest);
 		
 		int statusCode = response.getStatusLine().getStatusCode();
 		if (statusCode != 200) throw new Exception();
@@ -41,6 +41,13 @@ public class Utils {
 		user.setFelicaID(jsonObject.getString("felica_id"));
 			
 		return user;
+		
+	}
+	
+	public static User getUser(int userID) throws Exception {
+		final String url = "http://192.168.1.171:8080/api/users/" + String.valueOf(userID) + "/";
+		HttpGet httpGet = new HttpGet(url+"/");
+		return HTTPExcute(httpGet);
 	}
 	
 	static void deleteUser(int userID) {

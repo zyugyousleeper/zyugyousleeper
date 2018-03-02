bf71e4a7b21586054995e25fe78d00fc2bd6e254
ynakano1127 <ynakano1127@gmail.com>
2018-03-02 11:39:40 +0900

    [add]putとdeleteも

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -9,6 +9,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -51,21 +52,31 @@ public class Utils {
 	}
 	
 	public static User getUser(int userID) throws Exception {
-		final String url = "http://192.168.1.171:8080/api/users/" + String.valueOf(userID) + "/";
-		HttpGet httpGet = new HttpGet(url+"/");
+		final String url = "http://192.168.1.219:8080/api/users/" + String.valueOf(userID) + "/";
+		HttpGet httpGet = new HttpGet(url);
 		return HTTPExcute(httpGet);
 	}
 	
-	static void deleteUser(int userID) {
-		
+	public static void deleteUser(int userID) throws Exception {
+		final String url = "http://192.168.1.219:8080/api/users/" + String.valueOf(userID) + "/";
+		HttpDelete httpGet = new HttpDelete(url+"/");
+		HTTPExcute(httpGet);	
 	}
 	
-	static void putUser(int userID) {
+	static void putUser(User user) throws Exception {
+		final String url = "http://192.168.1.219:8080/api/users/" + String.valueOf(user.getStudentNum()) + "/";
+		HttpPost put = new HttpPost(url);
 		
+		ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("name", user.getName()));
+		params.add(new BasicNameValuePair("felica_id", user.getFelicaID()));
+		params.add(new BasicNameValuePair("user_id", String.valueOf(user.getStudentNum())));
+		
+		HTTPExcute(put);
 	}
 	
 	public static void postUser(User newUser) throws Exception{
-		final String url = "http://192.168.1.171:8080/api/users/" + String.valueOf(newUser.getStudentNum()) + "/";
+		final String url = "http://192.168.1.219:8080/api/users/" + String.valueOf(newUser.getStudentNum()) + "/";
 		HttpPost post = new HttpPost(url);
 		
 		ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();

01014d7c3215c40be6b043978acefd9ece76cb7c
ynakano1127 <ynakano1127@gmail.com>
2018-03-08 16:04:32 +0900

    [add]chargemodeを書いた

--- src/model/items/ChargeMode.java
+++ src/model/items/ChargeMode.java
@@ -2,18 +2,43 @@ package model.items;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import com.pi4j.io.serial.SerialDataEvent;
+import com.pi4j.io.serial.SerialDataEventListener;
+
+import model.CoinThread;
+import model.Money;
 import model.User;
 import ui.ModeFrame;
 import ui.PaymentPanel;
+import utils.Utils;
 
-public class ChargeMode extends ModeFrame implements Item {
+public class ChargeMode extends ModeFrame implements Item ,SerialDataEventListener {
 	private User user;
+	private Money money = new Money();
+	private PaymentPanel panel = new PaymentPanel();
+	private CoinThread coinThread = new CoinThread();
 	
 	public ChargeMode(User user) {
 		super();
 		this.user = user;
-		addPanel(new PaymentPanel());
+		panel.setMoneyKinds(Money.FIVEHUNDRED_YEN,Money.HUNDRED_YEN,Money.FIFTY_YEN,Money.TEN_YEN);
+		addPanel(panel);
+		
+		coinThread.setSerialDataEventListener(this);
+		coinThread.start();
+	}
+	
+	public void finel() {
+		user.setMoney(money);
+		try {
+			Utils.patchUser(user);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		coinThread.close();
 	}
 
 	@Override
@@ -29,5 +54,16 @@ public class ChargeMode extends ModeFrame implements Item {
 				setVisible(true);
 			}
 		};
+	}
+
+	@Override
+	public void dataReceived(SerialDataEvent arg0) {
+		try {
+			int kind = Integer.valueOf(arg0.getAsciiString());
+			money.addMoney(kind, 1);
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }

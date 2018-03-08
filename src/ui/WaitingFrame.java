43b2216aceaf92603b1353fe17715cf714cda4ec
ynakano1127 <ynakano1127@gmail.com>
2018-03-09 01:29:46 +0900

    [fix]waitingframeを直し

--- src/ui/WaitingFrame.java
+++ src/ui/WaitingFrame.java
@@ -2,6 +2,7 @@ package ui;
 
 import wrapper.FrameWrapper;
 import wrapper.LabelWrapper;
+import wrapper.PanelWrapper;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -11,16 +12,20 @@ import javax.swing.SwingConstants;
 public class WaitingFrame extends FrameWrapper {
 	public WaitingFrame() {
 		super();
-		getContentPane().setLayout(new BorderLayout(0, 0));
+		PanelWrapper panel = new PanelWrapper();
+		panel.setLayout(new BorderLayout(0, 0));
 		
 		LabelWrapper lblNewLabel = new LabelWrapper();
 		lblNewLabel.setText("カードを置いて下さい");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 60));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		panel.add(lblNewLabel, BorderLayout.CENTER);
 		
 		LabelWrapper lblNewLabel_2 = new LabelWrapper();
 		lblNewLabel_2.setText("授業SLEEPER");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(lblNewLabel_2, BorderLayout.SOUTH);
+		panel.add(lblNewLabel_2, BorderLayout.SOUTH);
+		
+		getContentPane().add(panel);
 	}
 }
--- src/wrapper/FrameWrapper.java
+++ src/wrapper/FrameWrapper.java
@@ -3,8 +3,10 @@ package wrapper;
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.border.EmptyBorder;
 
 public class FrameWrapper extends JFrame{
 	public FrameWrapper() {
+		setBackground(Color.decode("#606060"));
 	}
 }
